9835e3c6d831e0cc2987fc4f4a63747939069e6a
s9921316 <audiowalk.art@gmail.com>
2022-02-10 22:21:52 +0800

    make it better

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -4,6 +4,7 @@ module.exports = {
   PASSWORD: "0935451200",
   DB: "playgame",
   dialect: "mysql",
+  port: 3306,
   pool: {
     max: 5,
     min: 0,
@@ -11,3 +12,7 @@ module.exports = {
     idle: 10000
   }
 };
+
+// heroku config:set DATABASE_URL='mysql://pgadmin@us-cdbr-iron-east-02.cleardb.net/heroku_7643ec736354xxx?reconnect=true'
+
+// heroku config:add DATABASE_URL=mysql://pgadmin:0935451200@114.32.222.77/playgame
