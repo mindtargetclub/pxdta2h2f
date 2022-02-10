2e55353387f7317e771d29f0c68ac9121e4ac850
s9921316 <audiowalk.art@gmail.com>
2022-02-10 18:19:53 +0800

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "114.32.222.77",
+  USER: "pgadmin",
+  PASSWORD: "0935451200",
+  DB: "playgame",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
